9073c72b7981509e1806343ed23bda5d51185f0f
svol06 <svol06@vse.cz>
2021-10-24 19:45:25 +0200

    yaml overrides

--- server-deploy.sh
+++ server-deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo $1
 branch=$1
 echo $branch
 
@@ -14,28 +13,25 @@ cpref+="_"
 
 export PATHPREFIX="$ppref"
 export CONTAINERPREFIX="$cpref"
-export COMPOSE_FILE=docker-compose.caddy.yml
 echo $PATHPREFIX
 echo $CONTAINERPREFIX
-echo $COMPOSE_FILE
 
 if [ -d "git/$branch" ]; then
         echo "going to git/$branch"
         cd git/$branch
-        sudo -E docker-compose down
+        docker-compose down
         git pull
 else
         git clone -b $branch git@github.com:4IT580/joinme.git git/$branch
         cd git/$branch
 fi
 
-#cp /var/www/docker-compose.override.yml docker-compose.override.yml
+cp /var/www/docker-compose.override.yml docker-compose.override.yml
 
 if [ "$branch" = "master" ]; then
         export PATHPREFIX=""
         echo $PATHPREFIX
-        echo "I AM MASTER"
-        sudo -E docker-compose up -d
-else
-        sudo -E docker-compose up -d
+        echo "I AM MASTER"       
 fi
+
+docker-compose -f docker-compose.caddy.yml -f docker-compose.override.yml -f docker-compose.prod.yml up -d --build
