2276d010bfca06bbb317920638eefa87cdca464e
Adam Jedlička <adajedlicka@gmail.com>
2021-10-24 21:03:40 +0200

    Add greets

--- client/src/organisms/LandingHeader.jsx
+++ client/src/organisms/LandingHeader.jsx
@@ -2,10 +2,13 @@ import Button from '../atoms/Button'
 import Link from '../atoms/Link'
 import VerticalLine from '../atoms/VerticalLine'
 
-export default function LandingHeader() {
+export default function LandingHeader({ greet }) {
   return (
     <div className="flex flex-col-reverse md:flex-row justify-between p-6">
-      <h1 className="px-4 mt-8 text-6xl font-extrabold">Join.me!</h1>
+      <div>
+        <h1 className="px-4 mt-8 text-6xl font-extrabold">Join.me!</h1>
+        <h2 className="px-4 mt-4 text-2xl font-bold">{greet}</h2>
+      </div>
       <div className="flex self-end md:self-start">
         <div className="flex flex-col justify-center">
           <Link>Login</Link>
