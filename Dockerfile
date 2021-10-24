b2700cbbdb221316b44b76fceb2b98e61bc04728
Adam Jedlička <adajedlicka@gmail.com>
2021-10-24 21:16:56 +0200

    Add ENV to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM node:14-alpine
 ARG production
 ARG dir=/app
 
+ENV APOLLO_URL=http://localhost:8000
+
 WORKDIR $dir
 
 COPY package.json yarn.lock ./
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-console.log('APOLLO_URL', process.env.APOLLO_URL)
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -11,6 +9,6 @@ export default defineConfig({
     port: process.env.SERVER_PORT || 3000,
   },
   define: {
-    'process.env.APOLLO_URL': JSON.stringify(process.env.APOLLO_URL || 'http://localhost:8000'),
+    'process.env.APOLLO_URL': JSON.stringify(process.env.APOLLO_URL),
   },
 })
