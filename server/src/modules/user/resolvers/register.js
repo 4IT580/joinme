7876245f630ed3695466ed116a4a982e8747a2cb
Adam Jedlička <adajedlicka@gmail.com>
2021-11-08 15:32:44 +0100

    Refactoring

--- server/src/modules/user/resolvers/register.js
+++ server/src/modules/user/resolvers/register.js
@@ -1,12 +1,12 @@
-import { db } from '../../../lib/db.js'
+import * as yup from 'yup'
 import * as argon2 from 'argon2'
+import { randomBytes } from 'crypto'
+import { db } from '../../../lib/db.js'
 import * as token from '../../../lib/token.js'
 import * as mail from '../../../lib/mail.js'
-import * as yup from 'yup'
-import { randomBytes } from 'crypto'
 
 export default async (_, params) => {
-  try {
+  return await db().transaction(async (t) => {
     const loginSchema = yup.object({
       username: yup.string().min(3).max(20).required('Username is required'),
       name: yup.string().min(3).max(50).required('Name is required'),
@@ -17,16 +17,15 @@ export default async (_, params) => {
         .required('Password is required'),
     })
 
-    // validation will throw error, we do not need to save the result
     await loginSchema.validate(params)
 
-    const isUsernameTaken = await db().select('*').from('users').where('username', params.username).first()
+    const isUsernameTaken = await t.select('*').from('users').where('username', params.username).first()
 
     if (isUsernameTaken) {
       throw new Error('Username is taken')
     }
 
-    const isEmailTaken = await db().select('*').from('users').where('email', params.email).first()
+    const isEmailTaken = await t.select('*').from('users').where('email', params.email).first()
 
     if (isEmailTaken) {
       throw new Error('Email is taken')
@@ -37,25 +36,34 @@ export default async (_, params) => {
       password: await argon2.hash(params.password),
     }
 
-    const [id] = await db().insert(user).into('users')
+    const [id] = await t.insert(user).into('users')
 
     const ticket = {
       userId: id,
       secret: randomBytes(64).toString('base64'),
     }
 
-    await db().insert(ticket).into('userActivationTickets')
+    await t.insert(ticket).into('userActivationTickets')
 
     await mail.send({
       to: params.email,
       subject: 'Joinme registration',
-      html: `<p>Hello, ${params.name}! Click <a href="${
-        process.env.FRONTEND_URL
-      }/activate-account?secret=${encodeURIComponent(ticket.secret)}">here</a> to activate your account.</p>`,
+      html: getRegistrationMailContent({
+        name: params.name,
+        secret: ticket.secret,
+      }),
     })
 
-    return { user: await db().select('*').from('users').where('id', id).first(), token: token.create({ id: id }) }
-  } catch (e) {
-    throw new Error(e)
-  }
+    return {
+      user: await t.select('*').from('users').where('id', id).first(),
+      token: token.create({ id }),
+    }
+  })
+}
+
+const getRegistrationMailContent = ({ name, secret }) => {
+  const encodedSecret = encodeURIComponent(secret)
+  const link = `${FRONTEND_URL}/activate-account?secret=${encodedSecret}`
+
+  return `<p>Hello, ${name}! Click <a href="${link}">here</a> to activate your account.</p>`
 }
