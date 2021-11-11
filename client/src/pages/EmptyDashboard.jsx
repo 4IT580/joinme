f81b0c419f855943c7736a4196edac3cb405b2f4
svol06 <svol06@vse.cz>
2021-11-11 19:28:28 +0100

    renamed Dashboard to EmptyDashboard people should make new instances of Dashboard with content

--- client/src/pages/EmptyDashboard.jsx
+++ client/src/pages/EmptyDashboard.jsx
@@ -1,6 +1,6 @@
 import DashboardLayout from '../organisms/DashboardLayout'
 
-export default function Dashboard() {
+export default function EmptyDashboard() {
   return (
     <DashboardLayout>
       <h1 class="text-4xl font-bold mt-16 uppercase">There be lions</h1>
