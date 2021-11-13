f46c07463cc58cfaca09d89cb1c8cf4e67825e90
Dominik Botur <dom.botur@email.cz>
2021-11-13 23:48:05 +0100

    Slight design changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
         </div>
         <div className="grid grid-cols-5 gap-2 p-1">
           <div className="flex flex-col col-span-3 p-2">
-            <div className="flex flex-row items-center justify-between mb-4">
+            <div className="flex flex-row items-center justify-between mb-4 h-16">
               <h1 className="px-4 text-2xl font-extrabold">Events in your area</h1>
               <Button className="btn-primary uppercase">Create event</Button>
             </div>
@@ -47,28 +47,10 @@ export default function Dashboard() {
                 date="17.10 from 10am till 11pm"
                 image="https://picsum.photos/id/1005/400/250"
               />
-              <EventCard
-                title="COMIC-CON PRAGUE 2021"
-                venue="O2 universum"
-                date="17.10 from 10am till 11pm"
-                image="https://picsum.photos/id/1005/400/250"
-              />
-              <EventCard
-                title="COMIC-CON PRAGUE 2021"
-                venue="O2 universum"
-                date="17.10 from 10am till 11pm"
-                image="https://picsum.photos/id/1005/400/250"
-              />
-              <EventCard
-                title="COMIC-CON PRAGUE 2021"
-                venue="O2 universum"
-                date="17.10 from 10am till 11pm"
-                image="https://picsum.photos/id/1005/400/250"
-              />
             </div>
           </div>
           <div className="flex flex-col col-span-2 p-2">
-            <div className="flex flex-row items-center mb-4">
+            <div className="flex flex-row items-center mb-4 h-16">
               <h1 className="px-4 text-2xl font-extrabold">Places & Offers</h1>
             </div>
             <div className="grid grid-cols-1 gap-4">
