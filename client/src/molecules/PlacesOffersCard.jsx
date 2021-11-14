f7bc59b3f8b460b88199f415f6bb00c327eaed17
DomBot1 <67712182+DomBot1@users.noreply.github.com>
2021-11-14 15:01:15 +0100

    Add dashboard cards (#25)
    * Added "EventCard" and "PlacesOffersCard" atoms
    Updated dashboard
    Renamed "...rest" to "...props" in project files
    
    * Slight design changes
    
    * Refactoring
    
    Co-authored-by: Adam Jedlička <adajedlicka@gmail.com>

---
+++ client/src/molecules/PlacesOffersCard.jsx
@@ -0,0 +1,16 @@
+import classNames from 'classnames'
+
+export default function PlacesOffersCard({ title, address, description, image, className }) {
+  return (
+    <div className={classNames('card lg:card-side shadow-lg text-sm', className)}>
+      <figure className="p-4 flex items-center">
+        <img className="rounded-2xl h-36 w-36" src={image} />
+      </figure>
+      <div className="card-body">
+        <p className="pb-2">{description}</p>
+        <h2 className="card-title">{title}</h2>
+        <p>{address}</p>
+      </div>
+    </div>
+  )
+}
