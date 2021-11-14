c6878eb53f11db6a87782f99e5af472a7faf54a5
Adam Jedlička <adajedlicka@gmail.com>
2021-11-14 16:48:42 +0100

    Responsivity

--- client/src/molecules/PlacesOffersCard.jsx
+++ client/src/molecules/PlacesOffersCard.jsx
@@ -3,7 +3,7 @@ import classNames from 'classnames'
 export default function PlacesOffersCard({ title, address, description, image, className }) {
   return (
     <div className={classNames('card shadow-lg flex', className)}>
-      <div className="flex flex-row lg:flex-col xl:flex-row">
+      <div className="flex flex-col md:flex-row lg:flex-col xl:flex-row">
         <img className="rounded-2xl h-36 w-36 m-4 object-cover" src={image} />
         <p className="p-4">{description}</p>
       </div>
