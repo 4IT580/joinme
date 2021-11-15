de3510a85b92d8c5f172ab774fb320fe872eb700
Adam Jedlička <adajedlicka@gmail.com>
2021-11-15 11:17:55 +0100

    Notifications

---
+++ client/src/utils/notifications.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from 'react'
+
+const NotificationsContext = createContext()
+
+export function NotificationsProvider({ children }) {
+  const [notifications, setNotifications] = useState([])
+  const [id, setId] = useState(1)
+
+  const getId = () => {
+    const _id = id
+    setId(id + 1)
+    return _id
+  }
+
+  const pushNotification = (notification) => setNotifications([...notifications, { ...notification, id: getId() }])
+  const pushSuccess = (notification) => pushNotification({ title: 'Success', ...notification, type: 'SUCCESS' })
+  const pushError = (notification) => pushNotification({ title: 'Error', ...notification, type: 'ERROR' })
+  const remove = ({ id }) => setNotifications(notifications.filter((notification) => notification.id !== id))
+
+  const value = {
+    notifications,
+    pushSuccess,
+    pushError,
+    remove,
+  }
+
+  return <NotificationsContext.Provider value={value}>{children}</NotificationsContext.Provider>
+}
+
+export const useNotifications = () => useContext(NotificationsContext)
