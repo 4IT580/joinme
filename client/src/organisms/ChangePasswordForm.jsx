c7b31bed6e73e504bdd8140baa7a2ef1d4cf59ba
svol06 <svol06@vse.cz>
2021-11-28 19:41:34 +0100

    merge manual fixes

--- client/src/organisms/ChangePasswordForm.jsx
+++ client/src/organisms/ChangePasswordForm.jsx
@@ -1,74 +1,36 @@
 import * as yup from 'yup'
 import { Formik, Form } from 'formik'
-import { gql, useMutation } from '@apollo/client'
 import FormControl from '../molecules/FormControl'
 import Button from '../atoms/Button'
-import { useNotifications } from '../utils/notifications'
+import MultiInput from '../atoms/MultiInput'
 
-const CHANGE_PASSWORD_MUTATION = gql`
-  mutation ($oldPassword: String!, $newPassword: String!) {
-    changePassword(oldPassword: $oldPassword, newPassword: $newPassword)
-  }
-`
-
-const changePasswordFormSchema = yup.object().shape({
-  oldPassword: yup.string().required('Old password is required'),
-  newPassword: yup
-    .string()
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!\-@#\$%\^&\*])(?=.{8,})/,
-      'Password must contain at least 8 characters, a lowercase letter, an uppercase letter, a number and a special character',
-    )
-    .required('Password is required'),
-  passwordConfirmation: yup
-    .string()
-    .required('Password confirmation is required')
-    .oneOf([yup.ref('newPassword')], 'Passwords must match'),
+const updateProfileFormSchema = yup.object().shape({
+  name: yup.string().min(3).max(50).required(),
+  city: yup.string(),
+  description: yup.string(),
 })
 
-export default function UpdateUserProfileForm({ onClose }) {
-  const notifications = useNotifications()
-  const [changePassword, changePasswordState] = useMutation(CHANGE_PASSWORD_MUTATION)
-
+export default function UserProfileForm({ name, city, description, interests, onSubmit, onClose }) {
   return (
     <Formik
       initialValues={{
-        oldPassword: '',
-        newPassword: '',
-        passwordConfirmation: '',
+        name: name || '',
+        city: city || '',
+        description: description || '',
+        interests: interests || [],
       }}
-      validationSchema={changePasswordFormSchema}
-      onSubmit={async (variables) => {
-        try {
-          const { data, errors } = await changePassword({ variables })
-
-          if (data) {
-            notifications.pushSuccess({ text: 'Password change successfully' })
-            onClose()
-          } else {
-            for (const error of errors) {
-              notifications.pushError({ text: error.message })
-            }
-          }
-        } catch (e) {
-          notifications.pushError({ text: e.message })
-        }
-      }}
+      validationSchema={updateProfileFormSchema}
+      onSubmit={(variables) => onSubmit({ ...variables, interests: variables.interests.split(' ') })}
     >
       <Form>
-        <FormControl name="oldPassword" label="Old password" type="password" placeholder="Old password" />
-        <FormControl name="newPassword" label="New password" type="password" placeholder="Don't tell it to anyone" />
-        <FormControl
-          name="passwordConfirmation"
-          label="New password confirmation"
-          type="password"
-          placeholder="Repeat password to make sure it is without a typo"
-        />
-
+        <FormControl name="name" label="Name" placeholder="Your name" />
+        <FormControl name="city" label="City" placeholder="Your city" />
+        <FormControl name="description" label="Description" placeholder="Tell everyone about yourself"></FormControl>
+        <FormControl Component={MultiInput} name="interests" label="Interests" placeholder="Separate with spaces" />
         <div className="modal-action">
           <Button onClick={onClose}>Cancel</Button>
-          <Button type="submit" className="btn-primary" loading={changePasswordState.loading}>
-            Change password
+          <Button type="submit" className="btn-primary">
+            Submit
           </Button>
         </div>
       </Form>
