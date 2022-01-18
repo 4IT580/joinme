4368e26886bd2c40f43380e7b502d92fdf8190ae
Adam Jedlička <adajedlicka@gmail.com>
2022-01-18 14:28:48 +0100

    Sent invite link

--- server/src/modules/event/resolvers/shareEvent.js
+++ server/src/modules/event/resolvers/shareEvent.js
@@ -1,11 +1,18 @@
 import { db } from '../../../lib/db.js'
 import fetchOrCreateUser from '../services/fetchOrCreateUser.js'
+import sentInvitationLinkToEmail from '../services/sentInvitationLinkToEmail.js'
 
 export default async (_, { eventId, invites }) => {
   for (const invite of invites) {
     const user = await fetchOrCreateUser(invite)
 
     if (!user) throw new Error(`Unknown user ${invite}`)
+
+    const event = await db().select('*').from('events').where('id', eventId)
+
+    if (!event) throw new Error(`Unknown event with ID ${eventId}`)
+
+    await sentInvitationLinkToEmail(event, invite)
 
     try {
       await db().insert({ eventId, userId: user.id }).into('invitations')
